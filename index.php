f0392cb79de98bafd3d4fa0f1fb8655c54e09d5f
TheMiner63 <cboddy1@hotmail.com>
2016-06-07 13:20:52 +0100

    About | WSNC

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>About | WSNC</title>
+</head>
+<body>
+<h1>This page will be about WSNC team</h1>
+<a href="wsncoding.github.io">Return to homepage</a>
+
+</body>
+</html>
